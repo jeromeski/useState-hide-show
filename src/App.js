98f721d118c3c0dab33cb0d72541cf324596378b
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-04-05 19:52:01 +0800

    Updated repository

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+import { useState } from "react";
+import { Button, Card, CardBody, CardImg, CardText } from "reactstrap";
 
-export default function App() {
+export default function App({ posts }) {
+  const [openIndex, setOpenIndex] = useState(0);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      {posts.map(({ id, img, text }, idx) => {
+        const isOpen = openIndex === idx;
+        return (
+          <Card key={id}>
+            <CardImg top width="100%" src={img} />
+            <CardBody>
+              <CardText>
+                {isOpen ? text : text.substring(0, 100) + "..."}
+              </CardText>
+              {!isOpen && (
+                <Button onClick={() => setOpenIndex(idx)}>Open</Button>
+              )}
+            </CardBody>
+          </Card>
+        );
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
 
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App posts={[
+    {
+      id: 0,
+      img: 'https://ui.dev/images/content/code-splitting.png',
+      text: 'Code splitting has gained popularity recently for its ability to allow you to split your app into separate bundles your users can progressively load. In this post we’ll take a look at not only what code splitting is and how to do it, but also how to implement it with React Router.'
+    },
+    {
+      id: 1,
+      img: 'https://ui.dev/images/content/composition-vs-inheritance.png',
+      text: 'The problem with object-oriented languages is they’ve got all this implicit environment that they carry around with them. You wanted a banana but what you got was a gorilla holding the banana and the entire jungle. - Joe Armstrong.'
+    },
+    {
+      id: 2,
+      img: 'https://ui.dev/images/content/modules.png',
+      text: 'I’ve taught JavaScript for a long time to a lot of people. Consistently the most commonly under-learned aspect of the language is the module system. There’s good reason for that. Modules in JavaScript have a strange and erratic history. In this post we’ll walk through that history and you’ll learn modules of the past to better understand how JavaScript modules work today.'
+    }
+  ]} />,
   rootElement
 );
+
